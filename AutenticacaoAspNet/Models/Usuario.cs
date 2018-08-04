ecdc35cc16c83b5d5bece63a7be9f5eb94b9d837
Geovani Gonçalves Medeiros <geovaneindianopolis@gmail.com>
2018-08-04 01:19:47 -0300

    Login-Inicio

---
+++ AutenticacaoAspNet/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutenticacaoAspNet.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Login { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Senha { get; set; }
+    }
+}
---
+++ AutenticacaoAspNet/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(AutenticacaoAspNet.Startup))]
+
+namespace AutenticacaoAspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Autenticacao/login")
+            });
+        }
+    }
+}
